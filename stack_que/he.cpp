e4e7f3f5f7e4af1686e327688929952c63fb811f
rkcraushan333 <raushank.ug20.ece@nitp.ac.in>
2022-01-13 19:52:40 +0530

    check

---
+++ stack_que/he.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+signed main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        cout<<"HELLO WORLD"<<endl;
+    }
+    return 0;
+}
